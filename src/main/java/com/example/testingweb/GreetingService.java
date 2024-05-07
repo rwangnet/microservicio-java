f2e4fd127e85875f6c8cf6d0a8afda6e3d707b76
Wangnet, Ricardo <rwangnet@gmail.com>
2024-05-07 14:49:58 -0400

    Add initial commit for devops feature.
    Includes initial pipeline definition with gh actions

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 	public String greet() {
+		System.out.println("Generando una mala práctica aqui!");
 		return "Hello, World";
 	}
 }
