c1d3ea51cf581289ace06b504f42bf1000135f12
Wangnet, Ricardo <rwangnet@gmail.com>
2024-05-07 20:49:23 -0400

    Update GreetingService.java

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 	public String greet() {
-		System.out.println("Generando una mala práctica aqui!!!!!");
+		System.out.println("Generando una mala práctica aqui!!!!!!");
 		return "Hello, World";
 	}
 }
