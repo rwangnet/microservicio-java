a12cbba29cc36daf7984d30cf88baa8572a515d6
Wangnet, Ricardo <rwangnet@gmail.com>
2024-05-10 10:03:17 -0400

    Removing OCP configuration from pipeline. OCP SANDBOX doesnt support the required permissions for required Service account usage.

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Service;
 public class GreetingService {
 	public String greet() {
 		System.out.println("Generando una mala práctica aqui!!!!!!!!!!!!");
+
+		// Setting an explicit password to generate a security hotspot in the feature branch
+		String myDatabasePassword = "1234567890";
+
 		return "Hello, World";
 	}
 }
